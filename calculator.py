0fae7dac987191c1239a2881e5ab6452ea95f003
Cynthia878 <GitHub>
2025-04-14 23:26:59 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -15,21 +15,21 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or b <= 0 or a == 1:
         raise ValueError("Invalid arguments for logarithm.")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
