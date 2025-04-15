bc54f87833277451acdd2a6d6c04777dee8983a3
Cynthia878 <GitHub>
2025-04-14 23:14:39 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,23 +1,31 @@
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number.")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
+        raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
 
-def log(a, b):
-    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid logarithm input")
+def logarithm(a, b):
+    if a <= 0 or b <= 0 or a == 1:
+        raise ValueError("Invalid arguments for logarithm.")
     return math.log(b, a)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
