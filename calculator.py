f22107f75a3b8526ea8c4e1e53b358e2212a2082
Cynthia878 <GitHub>
2025-04-14 22:54:17 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -3,21 +3,21 @@ import math
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid base or value for logarithm")
+        raise ValueError("Invalid logarithm input")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
